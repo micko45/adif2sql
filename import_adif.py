a74279d6de5ce9daf007e62ce037886566d20b5d
micko45 <michaelmcgearty@gmail.com>
2024-09-23 14:35:37 +0100

    Update import_adif.py

--- import_adif.py
+++ import_adif.py
@@ -16,7 +16,8 @@ def connect_database():
             password=db_password,
             database=db_name,
             port=db_port,
-            use_pure=True  # Use pure Python MySQL connector
+            ssl_disabled=True  # Disable SSL if not needed
+
         )
         logging.info("Connected to the database.")
         return connection
