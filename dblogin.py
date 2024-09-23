183266dcbb43ae104b1ac9b2617f22de587de7d2
micko45 <michaelmcgearty@gmail.com>
2024-09-23 11:59:59 +0100

    Create dblogin.py

---
+++ dblogin.py
@@ -0,0 +1,9 @@
+import mysql.connector
+from mysql.connector import Error
+
+# Database configuration (change these according to your setup)
+db_host = 'localhost'
+db_user = 'root'
+db_password = 'letmein'
+db_name = 'qso_log'
+db_port = '3306'
